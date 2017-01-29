f2405e7d51cb4d514ca5486b5b52fbdc989d8557
Monirul Hasan <kmhasan@gmail.com>
2017-01-29 10:28:17 +0600

    Hello world

---
+++ Hello World Section 3/src/hello/world/section/pkg3/HelloWorldSection3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.section.pkg3;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class HelloWorldSection3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World\n");
+    }
+    
+}
