016f564d8a6bf4605a5c64f941047d90d4fe9441
kmhasan <kmhasan@SEUPC0719>
2017-01-30 09:21:27 +0600

    Hello World for Section 4

---
+++ Hello World Section 4/src/hello/world/section/pkg4/HelloWorldSection4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.section.pkg4;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class HelloWorldSection4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World\n");
+    }
+    
+}
