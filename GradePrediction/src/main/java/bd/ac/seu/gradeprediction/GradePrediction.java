5e30124fc396554445c1867464b6a5381d4f7a4b
kmhasan <kmhasan@SEUPC0719>
2017-02-06 11:26:11 +0600

    Maven MySQL project

--- GradePrediction/src/main/java/bd/ac/seu/gradeprediction/GradePrediction.java
+++ GradePrediction/src/main/java/bd/ac/seu/gradeprediction/GradePrediction.java
@@ -6,26 +6,38 @@
 package bd.ac.seu.gradeprediction;
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.*;
+
 /**
  *
  * @author kmhasan
  */
 public class GradePrediction {
 
-    private final String HOSTNAME = "172.17.0.119";
-    private final String USERNAME = "spring2017aj";
-    private final String PASSWORD = "aj";
-    private final String DBNAME = "predictdb";
-    private final String DBURL = "jdbc:mysql://" + HOSTNAME + "/" + DBNAME;
-    
     public GradePrediction() {
         try {
-            Connection connection = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
-            System.out.println("Connected");
+            Connection connection = ConnectionSingleton.getConnection();
+
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM student;";
+            
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            List<Student> studentsList = new ArrayList<>();
+            
+            while (resultSet.next()) {
+                int id = resultSet.getInt("studentId");
+                String name = resultSet.getString("studentName");
+                studentsList.add(new Student(id, name));
+            }
+            
+            for (Student student : studentsList)
+                System.out.println(student);
+            
+            // Add code to find out the student names that are
+            // more than 10 characters long
         } catch (SQLException ex) {
-            Logger.getLogger(GradePrediction.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(GradePrediction.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
---
+++ GradePrediction/src/main/java/bd/ac/seu/gradeprediction/Student.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.ac.seu.gradeprediction;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "studentId=" + studentId + ", studentName=" + studentName + '}';
+    }
+}
