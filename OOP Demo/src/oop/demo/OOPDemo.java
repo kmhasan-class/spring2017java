bc63f2289168e4b0aae59b747ff4e7d12ebc1423
kmhasan <kmhasan@SEUPC0719>
2017-02-13 09:47:23 +0600

    String to integer converter

--- OOP Demo/src/oop/demo/OOPDemo.java
+++ OOP Demo/src/oop/demo/OOPDemo.java
@@ -9,16 +9,35 @@ public class OOPDemo {
         return d;
     }
 
+    static int convertStringToInteger(String input) {
+        int sum = 0;
+        for (int i = 0; i < input.length(); i++) {
+            /*
+            sum = sum * 10;
+            char ch = input.charAt(i);
+            int digit = ch - '0';
+            sum = sum + digit;
+            */
+            sum = sum * 10 + input.charAt(i) - '0';
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         double x1 = 10, y1 = 5;
         double x2 = 3, y2 = 15;
         System.out.printf("Distance %f\n", getDistance(x1, y1, x2, y2));
-        
+
         // Point is a class (data type)
         // p1 is an object (variable)
         Point p1 = new Point(x1, y1);
         Point p2 = new Point(x2, y2);
-        System.out.printf("Distance %f\n", p1.getDistance(p2));
+        System.out.printf("Distance %.2f\n", p1.getDistance(p2));
+        
+        // Home Work: see if you can handle a + or - prefix
+        String str = "1356";
+        int x = convertStringToInteger(str);
+        System.out.println(x);
     }
 
 }
