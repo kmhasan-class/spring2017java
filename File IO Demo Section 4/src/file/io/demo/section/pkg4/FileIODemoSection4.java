d2d3b914f04ca0d1ead99950388b7b078ce95efc
kmhasan <kmhasan@SEUPC0719>
2017-03-01 09:44:50 +0600

    File IO

---
+++ File IO Demo Section 4/src/file/io/demo/section/pkg4/FileIODemoSection4.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file.io.demo.section.pkg4;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class FileIODemoSection4 {
+
+    public void readUsingScanner() {
+        int x;
+        double y;
+        System.out.println("Reading using Scanner");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an integer and a floating point number");
+        x = scanner.nextInt();
+        y = scanner.nextDouble();
+
+        System.out.println("integer x " + x);
+        System.out.println("double y " + y);
+    }
+
+    public void readUsingBufferedReader() {
+        int x;
+        double y;
+        System.out.println("Reading using BufferedReader");
+        BufferedReader bufferedReader;
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter an integer and a floating point number");
+        try {
+            x = Integer.parseInt(bufferedReader.readLine());
+            y = Double.parseDouble(bufferedReader.readLine());
+
+            System.out.println("integer x " + x);
+            System.out.println("double y " + y);
+        } catch (IOException ioe) {
+            System.err.println("Can't read from the keyboard anymore!");
+        }
+    }
+
+    public void readFromFile() {
+        RandomAccessFile randomAccessFile;
+        try {
+            randomAccessFile = new RandomAccessFile("input.txt", "r");
+            String line;
+
+            while (true) {
+                line = randomAccessFile.readLine();
+                if (line == null) {
+                    break;
+                }
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Could not find this file!");
+        } catch (IOException ioe) {
+            System.err.println("Can't read from that resource anymore!");
+        }
+    }
+
+    public void writeToFile() {
+        RandomAccessFile randomAccessFile;
+        try {
+            randomAccessFile = new RandomAccessFile("output.txt", "rw");
+            randomAccessFile.writeBytes("Hello world!\n");
+            randomAccessFile.writeBytes("Hi there!\n");
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Could not find this file!");
+        } catch (IOException ioe) {
+            System.err.println("Can't read from that resource anymore!");
+        }
+    }
+
+    public FileIODemoSection4() {
+//        readUsingScanner();
+//        readUsingBufferedReader();
+//        readFromFile();
+        writeToFile();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FileIODemoSection4 obj = new FileIODemoSection4();
+    }
+
+}
