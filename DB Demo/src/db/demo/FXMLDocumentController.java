379f121021bc32e29a606f388012345adae01119
kmhasan <kmhasan@SEUPC0719>
2017-03-15 12:34:12 +0600

    DB Demo for Section 4

--- DB Demo/src/db/demo/FXMLDocumentController.java
+++ DB Demo/src/db/demo/FXMLDocumentController.java
@@ -6,6 +6,11 @@
 package db.demo;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,14 +30,54 @@ public class FXMLDocumentController implements Initializable {
     private TextField studentNameField;
     @FXML
     private TextField cgpaField;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        try {
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://172.17.0.119/spring2017db",
+                            "javauser",
+                            "java");
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM student";
+            
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("studentId");
+                String name = resultSet.getString("studentName");
+                double cgpa = resultSet.getDouble("cgpa");
+                System.out.println("student id " + id);
+                System.out.println("student name " + name);
+                System.out.println("cgpa " + cgpa);
+            }
+        } catch (SQLException sqle) {
+            System.err.println("Could not connect to the database");
+        }            
+    }
 
     @FXML
     private void handleAddStudentAction(ActionEvent event) {
+        int studentId = Integer.parseInt(studentIdField.getText());
+        String studentName = studentNameField.getText();
+        double cgpa = Double.parseDouble(cgpaField.getText());
+
+        try {
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://172.17.0.119/spring2017db",
+                            "javauser",
+                            "java");
+            Statement statement = connection.createStatement();
+            String query = "INSERT INTO student VALUES(" + studentId 
+                    + ", '" + studentName + "', " + cgpa + ")";
+            System.out.println(query);
+            statement.executeUpdate(query);
+            
+            studentIdField.clear();
+            studentNameField.clear();
+            cgpaField.clear();
+        } catch (SQLException sqle) {
+            System.err.println("Could not connect to the database");
+        }
     }
-    
+
 }
