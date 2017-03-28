aab0871a2f7ac5d48728062011977747b15f4aa2
Monirul Hasan <kmhasan@gmail.com>
2017-03-28 12:44:07 +0600

    TableView Demo

--- Property Demo/src/property/demo/FXMLDocumentController.java
+++ Property Demo/src/property/demo/FXMLDocumentController.java
@@ -12,6 +12,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,7 +23,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
@@ -43,13 +48,25 @@ public class FXMLDocumentController implements Initializable {
     private int selectedIndex;
     @FXML
     private Label statusLabel;
-    
+    @FXML
+    private TableView<Property> ownersTableView;
+    @FXML
+    private TableColumn<Property, Number> propertyIdColumn;
+    @FXML
+    private TableColumn<Property, String> ownersNameColumn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ownersListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         ownersList = FXCollections.observableArrayList();
         ownersListView.setItems(ownersList);
+        ownersTableView.setItems(ownersList);
         //ownersList = new ArrayList<>();
+
+        // Lambda Expression
+        ownersNameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getOwnersName()));
+        propertyIdColumn.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getPropertyId()));
+
         try {
             Connection connection = DriverManager
                     .getConnection("jdbc:mysql://172.17.0.134/propertydb",
@@ -68,10 +85,9 @@ public class FXMLDocumentController implements Initializable {
 
         }
     }
-    
+
     // HOMETASK
     // try to do the update operation all by yourself
-
     @FXML
     private void handleSubmitAction(ActionEvent event) {
         // HOMETASK
@@ -97,30 +113,33 @@ public class FXMLDocumentController implements Initializable {
     // Write some code that lets you delete multiple items at once
     @FXML
     private void handleDeleteAction(ActionEvent event) {
-        try {
-            Connection connection = DriverManager
-                    .getConnection("jdbc:mysql://172.17.0.134/propertydb",
-                            "javauser",
-                            "java");
-            Statement statement = connection.createStatement();
-            int propertyId = ownersList.get(selectedIndex).getPropertyId();
-            String ownersName = ownersList.get(selectedIndex).getOwnersName();
-            statement.executeUpdate("DELETE FROM property WHERE propertyId = " 
-                    + propertyId + " AND ownersName = '" 
-                    + ownersName + "'");
-            ownersList.remove(selectedIndex);
-            statusLabel.setText("Deleted " + propertyId + " " + ownersName);
-            propertyIdField.clear();
-            ownersNameField.clear();
-        } catch (SQLException sqle) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Error message");
-            alert.setHeaderText("Database error");
-            alert.setContentText("Could not connect to the database");
-            alert.showAndWait();
-            System.err.println("Cannot connect to the database");
+        ObservableList<Integer> selectedIndices = ownersListView.getSelectionModel().getSelectedIndices();
+        
+        for (int i = 0; i < selectedIndices.size(); i++) {
+            try {
+                Connection connection = DriverManager
+                        .getConnection("jdbc:mysql://172.17.0.134/propertydb",
+                                "javauser",
+                                "java");
+                Statement statement = connection.createStatement();
+                int propertyId = ownersList.get(selectedIndices.get(i)).getPropertyId();
+                String ownersName = ownersList.get(selectedIndices.get(i)).getOwnersName();
+                statement.executeUpdate("DELETE FROM property WHERE propertyId = "
+                        + propertyId + " AND ownersName = '"
+                        + ownersName + "'");
+                ownersList.remove(selectedIndex);
+                statusLabel.setText("Deleted " + propertyId + " " + ownersName);
+                propertyIdField.clear();
+                ownersNameField.clear();
+            } catch (SQLException sqle) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error message");
+                alert.setHeaderText("Database error");
+                alert.setContentText("Could not connect to the database");
+                alert.showAndWait();
+                System.err.println("Cannot connect to the database");
+            }
         }
-
     }
 
     @FXML
@@ -128,6 +147,11 @@ public class FXMLDocumentController implements Initializable {
         selectedIndex = ownersListView.getSelectionModel().getSelectedIndex();
         propertyIdField.setText(ownersList.get(selectedIndex).getPropertyId() + "");
         ownersNameField.setText(ownersList.get(selectedIndex).getOwnersName());
+    }
+
+    @FXML
+    private void handleCloseAction(ActionEvent event) {
+        System.exit(0);
     }
 
 }
--- Property Demo/src/property/demo/PropertyDemo.java
+++ Property Demo/src/property/demo/PropertyDemo.java
@@ -22,7 +22,7 @@ public class PropertyDemo extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Property Database Application");
         stage.setScene(scene);
         stage.show();
     }
