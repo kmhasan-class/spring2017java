4c2a353a7602e2a6e9728c9a33ac67ad38e597d4
kmhasan <kmhasan@SEUPC0719>
2017-04-10 09:02:30 +0600

    Paint Program

--- Paint Program Section 4/src/paint/program/section/pkg4/FXMLDocumentController.java
+++ Paint Program Section 4/src/paint/program/section/pkg4/FXMLDocumentController.java
@@ -5,8 +5,15 @@
  */
 package paint.program.section.pkg4;
 
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,8 +23,12 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -37,10 +48,17 @@ public class FXMLDocumentController implements Initializable {
     private TextField brushSizeField;
     @FXML
     private Slider brushSizeSlider;
-    
+    @FXML
+    private ToggleButton polygonButton;
+    @FXML
+    private ToggleButton freehandButton;
+    private ArrayList<Double> xValues;
+    private ArrayList<Double> yValues;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        xValues = new ArrayList<>();
+        yValues = new ArrayList<>();
         isFirstLine = true;
         brushSizeSlider.setMin(10);
         brushSizeSlider.setMax(50);
@@ -48,6 +66,8 @@ public class FXMLDocumentController implements Initializable {
         gc = drawingCanvas.getGraphicsContext2D();
         gc.setLineWidth(brushSizeSlider.getValue());
         colorPicker.setValue(Color.BLACK);
+        freehandButton.setSelected(true);
+        polygonButton.setSelected(false);
     }    
 
     @FXML
@@ -55,7 +75,7 @@ public class FXMLDocumentController implements Initializable {
         double x = event.getX();
         double y = event.getY();
         
-        if (!isFirstLine)
+        if (!isFirstLine && freehandButton.isSelected())
             gc.strokeLine(oldX, oldY, x, y);
         
         isFirstLine = false;
@@ -89,6 +109,57 @@ public class FXMLDocumentController implements Initializable {
         brushSizeField.setText("" + brushSize);
         double strokeWidth = Double.parseDouble(brushSizeField.getText());
         gc.setLineWidth(strokeWidth);
+    }
+
+    @FXML
+    private void handlePolygonToggleAction(ActionEvent event) {
+        polygonButton.setSelected(true);
+        freehandButton.setSelected(false);
+    }
+
+    @FXML
+    private void handleFreehandToggleAction(ActionEvent event) {
+        polygonButton.setSelected(false);
+        freehandButton.setSelected(true);
+    }
+
+    @FXML
+    private void handlePolygonClickAction(MouseEvent event) {
+        if (polygonButton.isSelected()) {
+            if (event.isSecondaryButtonDown()) {
+                double[] xArray = new double[xValues.size()];
+                double[] yArray = new double[yValues.size()];
+                for (int i = 0; i < xArray.length; i++) {
+                    xArray[i] = xValues.get(i);
+                    yArray[i] = yValues.get(i);
+                }
+                gc.fillPolygon(xArray, yArray, xArray.length);
+            } else {
+                xValues.add(event.getX());
+                yValues.add(event.getY());
+                if (xValues.size() > 1) {
+                    gc.strokeLine(xValues.get(xValues.size() - 2), yValues.get(yValues.size() - 2),
+                            xValues.get(xValues.size() - 1), yValues.get(yValues.size() - 1));
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void handleFileSaveAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showSaveDialog(null);
+        
+        if (file != null) {
+            try {
+                WritableImage writableImage = new WritableImage((int) drawingCanvas.getWidth(), (int) drawingCanvas.getHeight());
+                drawingCanvas.snapshot(null, writableImage);
+                RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);            
+                ImageIO.write(renderedImage, "png", file);
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
 }
